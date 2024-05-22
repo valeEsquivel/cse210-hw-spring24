2223a5cd7756ba55c5077656221d6b58cf97305c
Valeria Esquivel <esq22001@byui.edu>
2024-05-21 23:13:30 -0600

    Polymorphism Learning Activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Circle(5, "Green"));
+        shapes.Add(new Rectangle(4, 5, "Blue"));
+        shapes.Add(new Square(4, "Purple"));
+
+        Console.Clear();
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}");
+        }
     }
 }
